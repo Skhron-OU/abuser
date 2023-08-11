b349c771cae89ea348f724f9d54a481313d34e43
Serg <g00g1@at.encryp.ch>
2023-08-11 16:10:36 +0000

    bugfix: retry failed RDAP queries

--- internal/resolveAbuseC/rdap.go
+++ internal/resolveAbuseC/rdap.go
@@ -5,6 +5,7 @@ import (
 	"abuser/internal/utils"
 	"fmt"
 	"net/netip"
+	"time"
 
 	"github.com/openrdap/rdap"
 )
@@ -63,30 +64,46 @@ func metaProcessor(abuseContacts *map[string]bool, entities *[]rdap.Entity) {
 }
 
 func ForIpByRDAP(ip netip.Addr) []string {
-	client := &rdap.Client{UserAgent: "SkhronAbuseComplainSender"}
-	ipMeta, err := client.QueryIP(ip.String())
+	var err error
+	var ipMeta *rdap.IPNetwork
+
+	client := &rdap.Client{UserAgent: "SkhronAbuseComplaintSender"}
+
+	for i := 0; i < 5 && err != nil; i++ {
+		ipMeta, err = client.QueryIP(ip.String())
+		time.Sleep(time.Second * time.Duration(i*2))
+	}
 
 	var abuseContacts map[string]bool = make(map[string]bool, 0)
 
 	if err == nil {
 		metaProcessor(&abuseContacts, &ipMeta.Entities)
+		// TODO: cache
 	} else {
-		l.Logger.Printf("%s, %s\n", ip.String(), err.Error())
+		l.Logger.Printf("[%s] RDAP query failed: %s\n", ip.String(), err.Error())
 	}
 
 	return utils.Keys(abuseContacts)
 }
 
 func ForAsnByRDAP(asn string) []string {
-	client := &rdap.Client{UserAgent: "SkhronAbuseComplainSender"}
-	asnMeta, err := client.QueryAutnum(asn)
+	var err error
+	var asnMeta *rdap.Autnum
+
+	client := &rdap.Client{UserAgent: "SkhronAbuseComplaintSender"}
+
+	for i := 0; i < 5 && err != nil; i++ {
+		asnMeta, err = client.QueryAutnum(asn)
+		time.Sleep(time.Second * time.Duration(i*2))
+	}
 
 	var abuseContacts map[string]bool = make(map[string]bool, 0)
 
 	if err == nil {
 		metaProcessor(&abuseContacts, &asnMeta.Entities)
+		// TODO: cache
 	} else {
-		l.Logger.Printf("%s, %s\n", asn, err.Error())
+		l.Logger.Printf("[%s] RDAP query failed: %s\n", asn, err.Error())
 	}
 
 	return utils.Keys(abuseContacts)
